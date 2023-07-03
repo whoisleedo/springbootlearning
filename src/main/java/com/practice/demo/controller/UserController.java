28be013bc49ada4d76c1761031883ee418714b38
Wilson <wilsonli@mitac.com.tw>
2023-07-03 13:33:16 +0800

    add resetPassword

--- src/main/java/com/practice/demo/controller/UserController.java
+++ src/main/java/com/practice/demo/controller/UserController.java
@@ -1,14 +1,13 @@
 package com.practice.demo.controller;
 
+
 import com.practice.demo.sevice.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api")
@@ -16,6 +15,7 @@ public class UserController {
     private static final Logger log = LoggerFactory.getLogger(UserController.class);
 
     private final UserService userService;
+
     @Autowired
     public UserController(UserService userService) {
         this.userService = userService;
@@ -23,12 +23,15 @@ public class UserController {
 
 
     @GetMapping("users/{id}")
-    public ResponseEntity<?> getAccountUser(@PathVariable long id){
+    public ResponseEntity<?> getAccountUser(@PathVariable long id , Authentication authentication){
+        String loginUserAccount =  (String)authentication.getPrincipal();
+        log.debug("user Account:{} query userId:{}" ,loginUserAccount,id);
         return userService.getUserById(id)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
 
     }
+
 
 
 }
--- src/main/java/com/practice/demo/entity/repository/AccountRepository.java
+++ src/main/java/com/practice/demo/entity/repository/AccountRepository.java
@@ -4,8 +4,11 @@ import com.practice.demo.entity.Account;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account,Long> {
 
     boolean existsByUserAccount(String account);
+    Optional<Account> findByUserAccount(String userAccount);
 }
