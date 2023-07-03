74fb59feb73b27811f30877b250d75830c96a615
Wilson <wilsonli@mitac.com.tw>
2023-07-03 13:09:25 +0800

    fix mapper miss account_account

--- src/main/java/com/practice/demo/util/JwtUtil.java
+++ src/main/java/com/practice/demo/util/JwtUtil.java
@@ -1,16 +1,13 @@
 package com.practice.demo.util;
 
-import com.practice.demo.entity.Account;
-import io.jsonwebtoken.Claims;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import com.practice.demo.entity.Account;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
@@ -20,39 +17,27 @@ public class JwtUtil {
     public static final String TOKEN_HEADER = "Authorization";
     public static final String TOKEN_PREFIX = "Bearer ";
     private static final String SECRET_KEY = "apC56MPCEENmNOTaY3liBeljGvmnLM2SZPVccp+WJW53xIe8l8qJLpgfqHgmBmYb5qtk8A1vYOnSDCY9S9BpVA==";
-    private static final long EXPIRATION_TIME = 86400000;
+    private static final long EXPIRATION_TIME = 300000;
+
 
     public static String generateToken(Account user) {
         Date now = new Date();
         Date expiration = new Date(now.getTime() + EXPIRATION_TIME);
         SecretKey secretKey = getKey();
-        Map<String,Object> claimMap = new HashMap<>();
-        claimMap.put("name", user.getName());
-        claimMap.put("email", user.getEmail());
-        claimMap.put("roles", "ROLE_USER");
+        Map<String,Object> detailMap = new HashMap<>();
+        detailMap.put("preferred_username", user.getUserAccount());
+        detailMap.put("email", user.getEmail());
+        detailMap.put("roles", "ROLE_USER");
 
         return Jwts.builder()
+                .addClaims(detailMap)
                 .setSubject(user.getName())
                 .setIssuer("demoApi")
-                .addClaims(claimMap)
                 .setIssuedAt(now)
                 .setExpiration(expiration)
-                .signWith(secretKey,SignatureAlgorithm.HS512)
+                .signWith(secretKey, SignatureAlgorithm.HS512)
                 .compact();
-    }
 
-
-    public static Claims getTokenBody(String token){
-        Claims claims = null;
-        try{
-            claims =Jwts.parserBuilder()
-                    .setSigningKey(getKey())
-                    .build().parseClaimsJws(token).getBody();
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-
-        return claims;
     }
 
     public static boolean validateToken(String token) {
@@ -60,7 +45,7 @@ public class JwtUtil {
                 Jwts.parserBuilder()
                         .setSigningKey(getKey())
                         .build()
-                        .parseClaimsJws(token.replace("Bearer ", ""))
+                        .parseClaimsJws(token.replace(TOKEN_PREFIX, ""))
                         .getBody()
                         .getSubject();
             }catch (Exception e){
@@ -79,14 +64,6 @@ public class JwtUtil {
     }
 
 
-    public static boolean isExpiration(String token){
-        try{
-            return getTokenBody(token).getExpiration().before(new Date());
-        }catch (Exception e){
-            e.printStackTrace();
-        }
-        return true;
-    }
     private static SecretKey getKey(){
         return Keys.hmacShaKeyFor(JwtUtil.SECRET_KEY.getBytes(StandardCharsets.UTF_8));
     }
@@ -94,8 +71,8 @@ public class JwtUtil {
 
     public static Authentication getAuthentication(String token) {
         Claims claims = parseToken(token);
+        String username = (String)claims.get("preferred_username");
 
-        String username = claims.getSubject();
         List<GrantedAuthority> authorities = Arrays.stream(claims.get("roles").toString().split(","))
                 .map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
