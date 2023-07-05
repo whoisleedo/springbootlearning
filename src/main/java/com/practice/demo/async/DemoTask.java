746b6d1bb398f2a0252deb8b8483555ab5036212
Wilson <wilsonli@mitac.com.tw>
2023-07-05 16:21:33 +0800

    add async task

---
+++ src/main/java/com/practice/demo/async/DemoTask.java
@@ -0,0 +1,54 @@
+package com.practice.demo.async;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadLocalRandom;
+
+@Component
+public class DemoTask {
+    private static final Logger log = LoggerFactory.getLogger(DemoTask.class);
+    @Qualifier("paramThreadPool")
+    @Autowired
+    private Executor executor;
+
+
+    public void demoTask(List<Integer> numList) {
+        CompletableFuture<?>[] futures = numList.stream()
+                .map(i -> CompletableFuture.runAsync(() -> calculateAndLog(i), executor))
+                .toArray(CompletableFuture[]::new);
+
+        CompletableFuture.allOf(futures)
+                .thenRun(() -> log.info("All tasks complete"))
+                .exceptionally(ex -> {
+                    log.error("Some tasks failed: " + ex.getMessage());
+                    return null;
+                });
+    }
+
+    private void calculateAndLog(int i){
+        try{
+            Thread.sleep(randomSleep());
+            int result = i * i;
+            log.info("The square of " + i + " is " + result);
+        }catch (InterruptedException e){
+            log.error(e.getMessage());
+            throw new RuntimeException(e.getMessage());
+        }
+
+    }
+
+    private long randomSleep(){
+        long min = 800;
+        long max = 2000;
+        return ThreadLocalRandom.current().nextLong(min, max + 1);
+
+    }
+}
---
+++ src/main/java/com/practice/demo/configuration/ExecutorConfiguration.java
@@ -0,0 +1,38 @@
+package com.practice.demo.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.ThreadPoolExecutor;
+
+@Configuration
+public class ExecutorConfiguration {
+
+    @Value("${demo.thread_pool.core_pool_size:2}")
+    private int corePoolSize;
+    @Value("${demo.thread_pool.max_pool_size:8}")
+    private int maxPoolSize;
+    @Value("${demo.thread_pool.queue_capacity:10}")
+    private int queueCapacity;
+    @Value("${demo.thread_pool.keep_alive_seconds:60}")
+    private int keepAliveSeconds;
+    @Value("${demo.thread_pool.thread_name_prefix:taskExecutor-}")
+    private String threadNamePrefix;
+
+    @Bean
+    public Executor paramThreadPool() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(corePoolSize);
+        executor.setMaxPoolSize(maxPoolSize);
+        executor.setQueueCapacity(queueCapacity);
+        executor.setKeepAliveSeconds(keepAliveSeconds);
+        executor.setThreadNamePrefix(threadNamePrefix);
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        return executor;
+    }
+
+
+}
