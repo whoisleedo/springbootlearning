94927d1f2741aac531e05f775620e4630d4f50f6
whoisleedo <78330111+whoisleedo@users.noreply.github.com>
2023-07-05 16:24:47 +0800

    add async task (#19)
    Co-authored-by: Wilson <wilsonli@mitac.com.tw>

---
+++ src/main/java/com/practice/demo/schedule/DemoScheduler.java
@@ -0,0 +1,58 @@
+package com.practice.demo.schedule;
+
+
+import com.practice.demo.async.DemoTask;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+@Component
+public class DemoScheduler {
+    private static final Logger log = LoggerFactory.getLogger(DemoScheduler.class);
+
+    private AtomicBoolean methodCheck;
+    @Autowired
+    private DemoTask demoTask;
+
+    @PostConstruct
+    public void initScheduler() {
+        methodCheck = new AtomicBoolean();
+    }
+
+    @Scheduled(cron = "${demo.scheduler.cron}")
+    public void demoSchedule() {
+        if (methodCheck.compareAndSet(false, true)) {
+            try {
+                List<Integer> numList = getNumList();
+                demoTask.demoTask(numList);
+            } catch (Exception e) {
+                log.error("task error");
+
+            } finally {
+                methodCheck.set(false);
+            }
+
+        }
+    }
+
+    private List<Integer> getNumList() {
+        Random random = new Random();
+        return IntStream.range(0, 50)
+                .map(i -> 1 + random.nextInt(20))
+                .boxed()
+                .collect(Collectors.toList());
+
+    }
+
+
+}
